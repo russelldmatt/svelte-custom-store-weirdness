333221d484c6a039fe658c2b5c0a498688114085
Matt Russell <russell.d.matt@gmail.com>
2023-01-05 10:10:13 -0800

    .

--- src/routes/persistentStore.ts
+++ src/routes/persistentStore.ts
@@ -1,19 +1,15 @@
 import type { Writable, Updater, Subscriber } from 'svelte/store';
 
 export function myWritable<T>(name: string, initialValue: T): Writable<T> {
-	console.log(`${name}'s initial value is ${initialValue}`);
 	let currentValue = initialValue;
-	let keyCounter = 0;
-	let subscriptions: Map<number, Subscriber<T>> = new Map();
+	let subscriptions: Set<Subscriber<T>> = new Set();
 
 	let subscribe = (f: Subscriber<T>) => {
-		console.log(`calling [f] for ${name} with currentValue ${currentValue}`);
-
 		f(currentValue);
-		let key = (keyCounter += 1);
-		subscriptions.set(key, f);
-		let unsubscribe = () => subscriptions.delete(key);
-		return unsubscribe;
+		subscriptions.add(f);
+		return function unsubscribe() {
+			subscriptions.delete(f);
+		};
 	};
 
 	function set(value: T) {
